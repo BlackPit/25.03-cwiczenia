ccc51870320dfe56ed5a0a146d42c9cc3f2b2f2a
Piotr Wonotrski <p.wontorski@gmail.com>
2019-03-25 15:39:08 +0100

    nowe zakaldki

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import javax.naming.Name;
+
 public class Main {
     public static void main(String[] args) {
 
+        Task1 task1 = new Task1();
+        Task1.run();
+
+
     }
-}
+
+    }
+
+
+
+
+
---
+++ src/Task1.java
@@ -0,0 +1,15 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Task1 {
+    public void run() {
+        public static void Task1 (String[] args){
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Wpisz imiona: ");
+            String name = scan.nextLine();
+
+            System.out.println("Twoje imiona: " + Task1(););
+
+        }
+    }
+}
