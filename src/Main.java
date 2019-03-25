6aa6e82fd30987273cc89a577a6121017969fe6e
Piotr Wonotrski <p.wontorski@gmail.com>
2019-03-25 16:46:36 +0100

    zmiana kolejna

--- src/Main.java
+++ src/Main.java
@@ -6,18 +6,7 @@ public class Main {
         Task1 task1 = new Task1();
         Task1.run();
 
-//        String[] daysOfWeek = new String[7];
-//        daysOfWeek[0] = "poniedziałek";
-//        daysOfWeek[1] = "wtorek";
-//        daysOfWeek[2] = "środa";
-//        daysOfWeek[3] = "czwartek";
-//        daysOfWeek[4] = "piątek";
-//        daysOfWeek[5] = "sobota";
-//        daysOfWeek[6] = "niedziela";
-//        System.out.println(daysOfWeek[5]);
-//
-//        String[] wintersMonths = new String[] {"grudzień", "styczeń", "luty"};
-//        System.out.println(wintersMonths.length);
+
 
 
     }
--- src/Task1.java
+++ src/Task1.java
@@ -7,14 +7,22 @@ public class Task1 {
             Scanner scan = new Scanner(System.in);
             System.out.println("Wpisz liczbe : ");
             int number = scan.nextInt();
-
-            
-
+            System.out.println("wpisales: " +number);
 
 
+            }
 
+            public String [] makeTable(int number){
+
+            String [] names = new String[number];
+            for (int i=0; i<number; i++){
+                int j = i+1
+                System.out.println("Podaj: " +j "liczbe");}
 
 
         }
+
+
+
     }
 
